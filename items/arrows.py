550386f9dd901b70e9e06afc4397f37f7b1c63e2
Artem <domrachev.artem@list.ru>
2025-04-28 18:47:41 +0300

    added arrows for archer class

---
+++ items/arrows.py
@@ -0,0 +1,44 @@
+from characters.base import ICharacter
+from items.base import IConsumable
+
+
+class Arrow(IConsumable):
+    def __init__(self, name: str, damage: int = 5):
+        self._name = name
+        self._damage = damage
+        self._max_stack = 30
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @property
+    def max_stack(self) -> int:
+        return self._max_stack
+
+    @property
+    def damage(self) -> int:
+        return self._damage
+
+    def consume(self) -> dict:
+        return {"damage": self._damage}
+
+    def apply_effect(self, target: "ICharacter") -> None:
+        pass
+
+
+class FrostArrow(Arrow):
+    def __init__(self):
+        super().__init__(name="Ледяная стрела", damage=10)
+
+    def apply_effect(self, target: "ICharacter") -> None:
+        print(f"{target.name} замедлен!")
+
+
+class FireArrow(Arrow):
+    def __init__(self):
+        super().__init__(name="Огненная стрела", damage=10)
+
+    def apply_effect(self, target: "ICharacter") -> None:
+        print(f"{target.name} горит и получает доп. урон!")
+        target.take_damage(5)  # Доп урон от горения
